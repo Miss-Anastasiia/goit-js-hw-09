8e4703f4fc549664cb29dcbdff0009e63f01162d
Miss-Anastasiia <110167985+Miss-Anastasiia@users.noreply.github.com>
2022-12-26 20:19:41 +0200

    HW9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,46 +8,33 @@ function createPromise(position, delay) {
 }
 
 import Notiflix from 'notiflix';
-import 'notiflix/dist/notiflix-3.2.5.min.css';
-
-const form = document.querySelector('.form');
-
-const firstDelayMs = document.querySelector('[name="delay"]');
-const delayStepMs = document.querySelector('[name="step"]');
-const amount = document.querySelector('[name="amount"]');
 
 
-form.addEventListener('submit', submitCreatePromises);
+const refs = {
+  form: document.querySelector('.form'),
+}
 
+refs.form.addEventListener('submit', onFormSubmit);
 
-function submitCreatePromises(e) {
-
+function onFormSubmit(e) {
   e.preventDefault();
+  
+  let mainDelay = e.currentTarget.delay.valueAsNumber;
+  const delayStep = e.currentTarget.step.valueAsNumber;
+  const amountOfPromise = e.currentTarget.amount.valueAsNumber;
+  
 
-  let delay = firstDelayMs.valueAsNumber;
-  const delayStepMsVal = delayStepMs.valueAsNumber;
-  const amountVal = amount.valueAsNumber;
-
-  for (let i = 1; i <= amountVal; i++) {
-    createPromise(i, delay)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${i} in ${delay}ms`
-        );
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${i} in ${delay}ms`
-        );
-      });
-    delay += delayStepMsVal;
-  }
+  for (let position = 1; position <= amountOfPromise; position += 1) {
+    createPromise(position, mainDelay);
+    mainDelay += delayStep;
+  };
 }
 
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
     setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
@@ -55,4 +42,10 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
+
+  promise.then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
